4501cdf3abf6446bb90174b44c2d889ff395e564
Afnan Tawsif <90197228+AfnanTawsif@users.noreply.github.com>
2026-08-13 18:08:49 +0600

    Bump cache version to v1.0.3

--- WebApp/App/sw.js
+++ WebApp/App/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ff-catalog-app-v1.0.2'; // Bump version for new assets
+const CACHE_NAME = 'ff-catalog-app-v1.0.3'; // Bump version for new assets
 const ASSETS = [
     './',
     './index.html',
